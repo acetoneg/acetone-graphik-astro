a50cb1bfceb5cbcce63e1212546d880eed2f7fdf
Balint Fulop <bfulop@mac.com>
2024-12-10 13:38:03 +0100

    netlify image cdn config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify/functions";
 
 export default defineConfig({
-  output: "server", // Required for Netlify Edge Functions
-  adapter: netlify({
-    imageCDN: true,
-  }),
+  output: "static",
+  adapter: netlify(),
   image: {
-    domains: [], // Add any external domains you want to allow
-    remotePatterns: [], // Add any remote patterns you want to allow
+    service: netlify(),
   },
 });
